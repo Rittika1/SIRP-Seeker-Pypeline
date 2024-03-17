c7e50d2a0871e39715210a4b0d0f04d1d077e782
Rittika Mallik <rmallik1@uncc.edu>
2024-03-16 20:02:31 -0400

    updated paralogs code

--- countparalogs.py
+++ countparalogs.py
@@ -9,11 +9,12 @@ countsdict = {}
 for line in inf:
     if line.startswith(">"):
         SplitLine = line.split("_")
-        speciesname = SplitLine[0][1:] +" " +SplitLine[1]
+        # print(SplitLine[1] + "__")
+        speciesname = SplitLine[0][1:] +"_" +SplitLine[1].rstrip()
         if speciesname in countsdict.keys():
             countsdict[speciesname]+=1
         else:
             countsdict[speciesname]=1
 
 for key,val in countsdict.items():
-    print( key, "\t", val)
+    print( key,"\t",val)
